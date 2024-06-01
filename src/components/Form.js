e0e8b159c763d84766ead562581c67c4af504fde
Adesodun Titilope Temiloluwa <115459326+adesodun27@users.noreply.github.com>
2024-06-01 13:48:01 +0100

    initial commit

---
+++ src/components/Form.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import styles from "./form.module.css";
+
+export default function Form({ todos, setTodos }) {
+  // const [todo, setTodo] = useState("");
+  const [todo, setTodo] = useState({ name: "", done: false });
+  function handleSubmit(e) {
+    e.preventDefault();
+    setTodos([...todos, todo]);
+    setTodo({ name: "", done: false });
+  }
+  return (
+    <form className={styles.todoform} onSubmit={handleSubmit}>
+      <div className={styles.inputContainer}>
+        <input
+          className={styles.modernInput}
+          placeholder="Enter todo item"
+          onChange={(e) => setTodo({name:e.target.value, done: false})}
+          value={todo.name}
+          type="text"
+        />
+        <button type="submit" className={styles.modernButton}>
+          Add
+        </button>
+      </div>
+    </form>
+  );
+}
